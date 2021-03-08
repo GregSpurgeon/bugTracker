4a1a9c40350f31d501e1ab94f0bd373ec09b8003
Greg Spurgeon <spurgeon.greg11@gmail.com>
2021-03-08 15:38:37 -0500

    moved secret key to .env file

--- bugs/forms.py
+++ bugs/forms.py
@@ -11,5 +11,3 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ("title", "description")
-        # title = forms.CharField(max_length=100)
-        # description = forms.CharField(widget=forms.Textarea, max_length=250)
